ba55274af6039dd3ae72fc08dbc70cf8d4e7c88d
Gernot Feichter <gernotfeichter@gmail.com>
2025-05-24 18:24:51 +0200

    feat(clevis): disable per default and make a single toggle

--- config/hypnix/hardware/default-machine-name/clevis-luks.nix
+++ config/hypnix/hardware/default-machine-name/clevis-luks.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, boot, ... }:
 let
+  enabled = false;
   interfaceWifi = "wlp0s20u5";
   interfaceEth = "enp7s0";
   luksDeviceList = builtins.attrNames config.boot.initrd.luks.devices;
@@ -13,44 +14,46 @@ let
   allKernelModulesList = map (x: builtins.elemAt (pkgs.lib.match ".*/(.*)\.ko" x) 0) kernelModulePaths;
   # code for generating allKernelModulesList - END
 in {
-  # kernel modules allowed to be loaded in initrd boot phase (all)
-  boot.initrd.availableKernelModules = allKernelModulesList;
+  mkIf enabled {
+    # kernel modules allowed to be loaded in initrd boot phase (all)
+    boot.initrd.availableKernelModules = allKernelModulesList;
 
-  # dhcp
-  networking.interfaces."${interfaceWifi}".useDHCP = true;
-  networking.interfaces."${interfaceEth}".useDHCP = true;
+    # dhcp
+    networking.interfaces."${interfaceWifi}".useDHCP = true;
+    networking.interfaces."${interfaceEth}".useDHCP = true;
 
-  # initrd general
-  boot.initrd.verbose = true;
-  boot.initrd.enable = true;
+    # initrd general
+    boot.initrd.verbose = true;
+    boot.initrd.enable = true;
 
-  # networking
-  boot.initrd.systemd.enable = true;
-  boot.initrd.network.enable = false;
-  boot.initrd.systemd.network.enable = false;
-  boot.initrd.systemd.network.wait-online.enable = true;
-  boot.initrd.systemd.emergencyAccess = true;
-  boot.initrd.systemd.initrdBin = [ pkgs.wpa_supplicant pkgs.coreutils pkgs.systemd ];
+    # networking
+    boot.initrd.systemd.enable = true;
+    boot.initrd.network.enable = true;
+    boot.initrd.systemd.network.enable = true;
+    boot.initrd.systemd.network.wait-online.enable = true;
+    boot.initrd.systemd.emergencyAccess = true;
+    boot.initrd.systemd.initrdBin = [ pkgs.wpa_supplicant pkgs.coreutils pkgs.systemd ];
 
-  # clevis
-  boot.initrd.clevis.enable = false;
-  boot.initrd.clevis.useTang = true;
-  boot.initrd.clevis.devices."luks-${luksDeviceId}".secretFile = secrets/luks.jwe;
-  boot.initrd.systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
+    # clevis
+    boot.initrd.clevis.enable = true;
+    boot.initrd.clevis.useTang = true;
+    boot.initrd.clevis.devices."luks-${luksDeviceId}".secretFile = secrets/luks.jwe;
+    boot.initrd.systemd.users.root.shell = "${pkgs.bash}/bin/bash";
 
-  # wifi
-  # https://discourse.nixos.org/t/wireless-connection-within-initrd/38317/13
-  boot.initrd.systemd.packages = [ pkgs.wpa_supplicant ];
-  boot.initrd.systemd.targets.initrd.wants = [ "wpa_supplicant@${interfaceWifi}.service" "systemd-resolved.service" ];
-  boot.initrd.systemd.services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
-  boot.initrd.secrets."/etc/wpa_supplicant/wpa_supplicant-${interfaceWifi}.conf" = secrets/wpa_supplicant.conf;
+    # wifi
+    # https://discourse.nixos.org/t/wireless-connection-within-initrd/38317/13
+    boot.initrd.systemd.packages = [ pkgs.wpa_supplicant ];
+    boot.initrd.systemd.targets.initrd.wants = [ "wpa_supplicant@${interfaceWifi}.service" "systemd-resolved.service" ];
+    boot.initrd.systemd.services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
+    boot.initrd.secrets."/etc/wpa_supplicant/wpa_supplicant-${interfaceWifi}.conf" = secrets/wpa_supplicant.conf;
 
-  # ethernet
-  boot.initrd.systemd.network.networks."10-wlan" = {
-    matchConfig.Name = interfaceEth;
-    networkConfig.DHCP = "yes";
-  };
+    # ethernet
+    boot.initrd.systemd.network.networks."10-wlan" = {
+      matchConfig.Name = interfaceEth;
+      networkConfig.DHCP = "yes";
+    };
 
-  # dns
-  boot.initrd.services.resolved.enable = true;
+    # dns
+    boot.initrd.services.resolved.enable = true;
+  }
 }
