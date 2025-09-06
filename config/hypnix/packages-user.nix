d2c5e6b196847dc414b4bacf1617ffcdb0d00580
Gernot Feichter <gernotfeichter@gmail.com>
2025-09-06 19:10:08 +0200

    fix: remove devlop

--- config/hypnix/packages-user.nix
+++ config/hypnix/packages-user.nix
@@ -3,32 +3,14 @@
   users.users."${config.hypnix.standardUser}".packages = with pkgs; [
       # For a list of available packages,see: https://search.nixos.org/packages
       firefox
-      neofetch
-      lolcat
       kate
       google-chrome
       libreoffice-qt
-      go
-      flutter
-      kubectl
-      kubernetes-helm
-      goreleaser
-      vscode
       vlc
       wev
-      ripgrep
-      gdb
       playerctl
-      gnome-multi-writer
       wlsunset
       gimp
       hyprshot
-      android-studio
-      android-tools
-      sdkmanager
-      nmap
-      jdk21
-      bundletool
-      python3
    ];
 }
